49d43fe7ce9a08de36d7e7f01a1590f95549a0ed
arturo <arturo@openframeworks.cc>
2014-04-16 11:10:51 +0200

    added docs

--- src/ofxXMPP.h
+++ src/ofxXMPP.h
@@ -121,32 +121,79 @@ struct ofxXMPPJingleHash{
 	string algo;
 };
 
+/// XMPP is a protocol that allows sending messages among to peers through a server
+/// It's used among others by jabber or google talk. Apart from chat like messages
+/// it allows to send metadata, through the subprotocol jingle, that enables both
+/// peers to start a connection to send video, audio... among each other.
+/// This class makes it easy to use XMPP and also implements part of the jingle protocol
+/// allowing other applications or addons like ofxGstRTP to start a video/audio conference
 class ofxXMPP: public ofThread {
 public:
 	ofxXMPP();
 	virtual ~ofxXMPP();
 
+	/// sets the show state of the client, this will tipically change the state icon in clients
+	/// as available, away, do not disturb...
 	void setShow(ofxXMPPShowState showState);
+
+	/// string that sets the status of the client, will show to other clients as a text message
+	/// near the client name
 	void setStatus(const string & status);
+
+	/// capabilities string that signals to other clients if this client has camera, microphone...
+	/// although there are some standard capabilities defined in the jingle RFC 2 clients can use
+	/// any string that they agree on to signal that they can start a connection for certain stream
+	/// types
 	void setCapabilities(const string & capabilities);
 
+	/// starts a connection to an XMPP server, passing the host to connect to, the username or jid
+	/// and the password
 	void connect(const string & host, const string & jid, const string & pass);
+
+	/// stops the connection
 	void stop();
 
+	/// sends a message to an specific client, to has to be the username or the specific id of a client
+	/// which is usually the username + a hash that we get calling getFriends()
 	void sendMessage(const string & to, const string & message);
+
+	/// returns a vector with all the users in our contact list from which we can know their state,
+	/// capabilities...
 	vector<ofxXMPPUser> getFriends();
 
+	/// returns the connection state of this client
 	ofxXMPPConnectionState getConnectionState();
 
+	/// returns the JID of this client as returned by the server usually the username + a hash
 	string getBoundJID();
 
-	/// general events
+	// general events
+
+	/// ofAddListener to this event to receive notifications about changes in the connection state
+	/// of this client
 	ofEvent<ofxXMPPConnectionState> connectionStateChanged;
+
+	/// ofAddListener to this event to receive new messages from other clients
 	ofEvent<ofxXMPPMessage> newMessage;
+
+	/// ofAddListener to this event to receive notifications about users in ourcontact list that
+	/// connect while we are connected
 	ofEvent<ofxXMPPUser> userConnected;
+
+	/// ofAddListener to this event to receive notifications about users in out conatct list
+	/// that disconnect while we are connected
 	ofEvent<ofxXMPPUser> userDisconnected;
 
 	/// jingle rtp events
+	/// this events are mostly used by ofxGstRTP to initiate video/audio connections
+	/// with other clients using the jingle protocol but can be used manually:
+	///
+	/// TODO: check this is correct
+	/// usually subscribe to all this events to implement the full jingle workflow,
+	/// when a user wants to start a connection with us we'll receive a jingleInitiationReceived
+	/// event then we need to call ack() passing the initiation that we just received wait for the
+	/// jingleRing event and then the application should do something to notify the user of the
+	/// incomming call if the user accepts the call we shoukd call ackRing
 	ofEvent<ofxXMPPJingleInitiation> jingleInitiationReceived;
 	ofEvent<string> jingleInitiationACKd;
 	ofEvent<ofxXMPPJingleInitiation> jingleInitiationAccepted;
